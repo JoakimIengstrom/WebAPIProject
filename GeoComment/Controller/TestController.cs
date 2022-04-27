764cd927c5a1abb84dd0cbb4aff184b38e5a6e90
JoakimIEngstrom <joakim.engstrom@utb.ecutbildning.se>
2022-04-27 10:47:48 +0200

    Test one sucess, status 200

--- GeoComment/Controller/TestController.cs
+++ GeoComment/Controller/TestController.cs
@@ -1,6 +1,25 @@
-﻿namespace GeoComment.Controller
+﻿using GeoComment.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoComment.Controller
 {
-    public class TestController
+    [Route("test")]
+    public class TestController : ControllerBase
     {
+        private readonly DatabaseHandler _databaseHandler;
+
+        public TestController(DatabaseHandler databaseHandler)
+        {
+            _databaseHandler = databaseHandler;
+        }
+
+        [Route("reset-db")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> ResetDB()
+        {
+            await _databaseHandler.ResetDB();
+            return Ok();
+        }
     }
 }
--- GeoComment/Models/Comment.cs
+++ GeoComment/Models/Comment.cs
@@ -3,5 +3,10 @@
     public class Comment
     {
         public int Id { get; set; }
+        public string Author { get; set; }
+        public string Message { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+
     }
 }
