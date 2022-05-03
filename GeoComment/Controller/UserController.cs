283c6c548ff45529d823d4010b707efd2c9a3021
JoakimIEngstrom <joakim.engstrom@utb.ecutbildning.se>
2022-05-03 14:59:56 +0200

    Klar med Bill User.

---
+++ GeoComment/Controller/UserController.cs
@@ -0,0 +1,49 @@
+﻿using GeoComment.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoComment.Controller
+{
+    [Route("api/user/register")]
+    [ApiVersion("0.2")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser(NewUser newUser)
+        {
+            var user = new User()
+            {
+                UserName = newUser.Username
+            };
+
+            await _userManager.CreateAsync(user, newUser.Password);
+
+            var registrationSuccess =
+                await _userManager.CheckPasswordAsync(user,
+                    newUser.Password);
+
+            if (!registrationSuccess)
+            {
+                return BadRequest(); //statuscode 400
+            }
+
+            var createdUser =
+                await _userManager.FindByNameAsync(user.UserName);
+            return Created("",
+                new
+                {
+                    username = createdUser.UserName, id = createdUser.Id
+                });
+
+        }
+    }
+}
---
+++ GeoComment/Models/NewUser.cs
@@ -0,0 +1,8 @@
+﻿namespace GeoComment.Models
+{
+    public class NewUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
